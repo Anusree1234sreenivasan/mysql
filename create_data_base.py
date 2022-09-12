36f039d363f0b2391213a065e21b0ae6c0f8e488
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-09-12 14:34:27 +0530

    FROUTH COMMIT

---
+++ create_data_base.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+
+conn = mysql.connector.connect(user='root',password='Anu@123sreenivasan', host='127.0.0.1')
+
+cursor = conn.cursor()
+cursor.execute("DROP database IF EXISTS MyDatabase")
+
+sql = "CREATE database MYDATABASE"
+
+cursor.execute(sql)
+
+print("List of databases: ")
+cursor.execute("SHOW DATABASES")
+print(cursor.fetchall())
+
+conn.close()
+
+
+
