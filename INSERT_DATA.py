11cacaceefab92f07c2550dd298f9fee4d7276da
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-09-12 14:32:36 +0530

    SECOND COMMIT

---
+++ INSERT_DATA.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    user='root',password='Anu@123sreenivasan', host='127.0.0.1',database='mysql')
+
+cursor = conn.cursor()
+
+sql = """ INSERT INTO EMPLOYEE(
+     FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)
+     VALUES('Mac', 'mohan', 20, 'M', 2000)"""
+try:
+    cursor.execute(sql)
+    conn.commit()
+    print("Data Inserted")
+except:
+    conn.rollback()
+conn.close()
+
+
